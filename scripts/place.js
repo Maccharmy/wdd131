9ca04dece734fa0b3219d1727a33883d287b212f
Maccharmy <musanga@byupathway.edu>
2026-05-18 13:28:46 +0100

    ouch

--- scripts/place.js
+++ scripts/place.js
@@ -1,20 +1,26 @@
-const year = document.querySelector('#year');
-const lastModified = document.querySelector('#lastModified');
-year.textContent = new Date().getFullYear();
-lastModified.textContent = `Last Modified: ${new Date().toLocaleString()}`;
+// Footer Dates
 
-// Wind chill calculation
-const temp = parseFloat(document.querySelector('#temp').textContent);
-const speed = parseFloat(document.querySelector('#speed').textContent);
-const windchillElement = document.querySelector('#windchill');
+document.querySelector("#year").textContent =
+    new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
+
+
+// Wind Chill Calculation
+
+const temperature = 24;
+const windSpeed = 6;
 
 function calculateWindChill(t, s) {
-    // Celsius formula
-    return (13.12 + 0.6215 * t - 11.37 * Math.pow(s, 0.16) + 0.3965 * t * Math.pow(s, 0.16)).toFixed(1);
+    return (13.12 + 0.6215 * t - 11.37 * s ** 0.16 + 0.3965 * t * s ** 0.16).toFixed(1);
 }
 
-if (temp <= 10 && speed > 4.8) {
-    windchillElement.textContent = `${calculateWindChill(temp, speed)}°C`;
+const windChill = document.querySelector("#windchill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent =
+        `${calculateWindChill(temperature, windSpeed)}°C`;
 } else {
-    windchillElement.textContent = 'N/A';
+    windChill.textContent = "N/A";
 }

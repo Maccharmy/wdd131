3369a988c4f80f67623fb6f9c11884c366eaebeb
Maccharmy <musanga@byupathway.edu>
2026-05-18 13:00:12 +0100

    latest

--- scripts/place.js
+++ scripts/place.js
@@ -3,13 +3,11 @@ const lastModified = document.querySelector('#lastModified');
 year.textContent = new Date().getFullYear();
 lastModified.textContent = `Last Modified: ${new Date().toLocaleString()}`;
 
-// Wind chill calculation
 const temp = parseFloat(document.querySelector('#temp').textContent);
 const speed = parseFloat(document.querySelector('#speed').textContent);
 const windchillElement = document.querySelector('#windchill');
 
 function calculateWindChill(t, s) {
-    // Celsius formula
     return (13.12 + 0.6215 * t - 11.37 * Math.pow(s, 0.16) + 0.3965 * t * Math.pow(s, 0.16)).toFixed(1);
 }
 
